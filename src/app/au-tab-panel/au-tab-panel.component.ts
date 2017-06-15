77c145634a0bff57ab7159c021f05077a592eb23
Zhentian Wan <answer881215@gmail.com>
2017-06-15 14:01:56 +0300

    ContentChildren, QueryList with AfterContentInit

--- src/app/au-tab-panel/au-tab-panel.component.ts
+++ src/app/au-tab-panel/au-tab-panel.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterContentInit, Component, ContentChildren, OnInit, QueryList} from '@angular/core';
+import {AuTabComponent} from '../au-tab/au-tab.component';
 
 @Component({
   selector: 'au-tab-panel',
   templateUrl: './au-tab-panel.component.html',
   styleUrls: ['../tab-panel.component.scss']
 })
-export class AuTabPanelComponent implements OnInit {
+export class AuTabPanelComponent implements OnInit, AfterContentInit {
+
+
+  @ContentChildren(AuTabComponent)
+  tabs: QueryList<AuTabComponent>;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ngAfterContentInit(): void {
+    console.log(this.tabs);
   }
 
 }
